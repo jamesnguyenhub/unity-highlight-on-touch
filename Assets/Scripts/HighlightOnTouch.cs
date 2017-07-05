eb733b75a8c8c46cb90902175e63beeccfd47ac8
Tuyen Nguyen <nntuyen92@gmail.com>
2017-07-05 08:50:44 +0700

    Script highlight

--- Assets/Scripts/HighlightOnTouch.cs
+++ Assets/Scripts/HighlightOnTouch.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HighlightOnTouch : MonoBehaviour {
+	private Material material;
 	private Color normalColor;
 	private Color highlightColor;
+	private bool touching;
+	private float interval;
 
 	void Start() {
-		Material material = GetComponent<MeshRenderer>().material;
-		normalColor = material.color;
+		material = GetComponent<MeshRenderer>().material;
 
+		normalColor = material.color;
 		highlightColor = new Color(
-			normalColor.r * 0.5f,
-			normalColor.g * 0.5f,
-			normalColor.b * 0.5f
+			normalColor.r * 1.5f,
+			normalColor.g * 1.5f,
+			normalColor.b * 1.5f
 		);
 	}
 
 	void Update() {
-		if (Input.GetMouseButtonDown(0)) {
-			
+		if (touching) {
+			interval += Time.deltaTime;
+			material.color = Color.Lerp(normalColor, highlightColor, interval);
 		}
+
+		if (Input.GetMouseButtonUp(0) && touching) {
+			touching = false;
+			material.color = normalColor;
+		}
+	}
+
+	void OnMouseDown() {
+		touching = true;
+		interval = 0f;
 	}
 }
