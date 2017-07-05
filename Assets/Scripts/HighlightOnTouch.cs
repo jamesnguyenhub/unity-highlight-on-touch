8ba235f726e0155b7b674ac210040f82fe90edc0
Tuyen Nguyen <nntuyen92@gmail.com>
2017-07-05 08:30:31 +0700

    Initial commit

---
+++ Assets/Scripts/HighlightOnTouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightOnTouch : MonoBehaviour {
+	private Color normalColor;
+	private Color highlightColor;
+
+	void Start() {
+		Material material = GetComponent<MeshRenderer>().material;
+		normalColor = material.color;
+
+		highlightColor = new Color(
+			normalColor.r * 0.5f,
+			normalColor.g * 0.5f,
+			normalColor.b * 0.5f
+		);
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			
+		}
+	}
+}
